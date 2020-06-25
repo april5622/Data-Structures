46e86522c13e1a28e83353ebab0c452a9182d9aa
April Ma <54913304+april5622@users.noreply.github.com>
2020-06-25 14:48:23 -0700

    part 1 of binary search tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -19,56 +19,53 @@ class BSTNode:
     def insert(self, value):
         # take the current value of our node (self.value)
         # compare to the new value we want to insert
-        if value == self.value:
-            return False
-          # if new value < self.value
+        if value < self.value:
+             # if new value < self.value
           # IF, self.left is already taken by a node,
             # make that left node, call insert
           # set the left to the new node with the new value
-        elif value < self.value:
-            if self.left:
-                return self.left.insert(value)
+            if self.left is None:
+                self.left = BSTNode(value) # this will replace the current left node and we dont want that
             else:
-                self.left = BSTNode(value)
-                return True
-          # if new value >= self.value 
+                self.left.insert(value) # recursion 
+                
+        if value >= self.value:
+            # if new value >= self.value 
             # IF, self.right is already taken by a node,
                 # make that right node, call insert
             # set the right child to the new node with the new value
-        else:
-            if self.right:
-                return self.right.insert(value)
+            if self.right is None: # if there is no node there, have that current right subroot node decide...
+                self.right = BSTNode(value) #...where to put the new value
             else:
-                self.right = BSTNode(value)      
-                return True
+                self.right.insert(value) # if the value is greater than the current, it will be inserted there
             
-     # False if tree does not contain value
+
      # Return True if the tree contains the value
+     # False if tree does not contain value
     def contains(self, target):
         # if current self.value == target:
         if self.value == target:
             # return True
             return True
         # compare the target to current value
-        # if current self.value < target
-        if self.value < target:
+        # if current self.value is more than target
+        if self.value >= target:
             # check the left subtree
-            if self.left is None:
+            if self.left is None: # left subtree not there
                 # if cannot go left, return False
                 return False
             found = self.left.contains(target) # recursion state
             
-        # if current self.value >= target
-        if self.value >= target:
-            # check the right subtrree contains target
-            if self.right is None:
+        # if current self.value is less than target
+        if self.value < target:
+            # check the right subtree contains target
+            if self.right is None: # right subtree not there
                 # if you cannt go right, return False
                 return False
-            found =  self.right.contains(target)
+            found =  self.right.contains(target) # recursion state
          # return if found since its true that in the recursion, it did contain the target
         return found
-            
-       
+
 
     # Return the maximum value found in the tree
     def get_max(self):
@@ -80,23 +77,51 @@ class BSTNode:
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
-        pass
+        # call the function on the current value fn(self.value)
+        fn(self.value)
+        # if you can go left, call for_each on the left tree
+        if self.left:
+            self.left.for_each(fn)
+        # if you can go right, call for_each on the right tree
+        if self.right:
+            self.right.for_each(fn)
+        
 
     # Part 2 -----------------------
 
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
-    def in_order_print(self, node):
-        pass
+    def in_order_print(self, node): # very similiar to for_each function
+        # print left then node then right
+        if self.left: # lowest value in the left first then root and then greatest value in the right
+            self.left.for_each(node)
+        node(self.value)
+        if self.right:
+            self.right.for_each(node)
+        
 
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
     def bft_print(self, node):
+        # create a queue for nodes                  
+        # add the first node to the queue
+        # while queue is not empty
+            # remove the first node from the queue
+            # print the remove node (Parent nodes are printed before children since its going in a wave)
+            # add all children into the queue
         pass
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
-    def dft_print(self, node):
+    def dft_print(self, node): # this is like for_each function of printing
+        # create a stack for nodes
+        # add th first node to the stack
+        # while stack is not empty
+            # get current node from the top of stack
+            # print node remove from stack from top to bottom
+            # add all the children to that stack (left children first then go back up to right subroot and its children)
+            # keep in mind, the order you add the children will matter. greatest to smallest
+        
         pass
 
     # Stretch Goals -------------------------
